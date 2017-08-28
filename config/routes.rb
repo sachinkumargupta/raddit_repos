b12e0c0b4edcec0c6b0c099ea79953e62b494ff2
sachinkumargupta <sachin7870020331@gmail.com>
2017-08-28 22:51:06 +0530

    Add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :links do 
     member do 
       put "Like", to: "links#upvote"
       put "Dislike", to: "links#downvote"
     end 
+    resources :comments
   end
 
   root "links#index"
